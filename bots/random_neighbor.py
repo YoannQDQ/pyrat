9e029ba5c1350156f2c72b5fe564b48c91734ef1
Yoann Quenach de Quivillic <yquenach@itlink.fr>
2025-11-07 11:18:31 +0100

    Remove bots

--- bots/random_neighbor.py
+++
@@ -1,9 +0,0 @@
-import random
-
-from bots.utils import direction
-
-
-def go(neighbors_map, player_location, pieces_of_cheese):
-    choices = neighbors_map[player_location]
-    target = random.choice(choices)
-    return direction(player_location, target)
--- bots/utils.py
+++ bots/utils.py
@@ -1,17 +1,7 @@
 from pyrat.core.bot_utils import MOVE_E, MOVE_N, MOVE_O, MOVE_S
 
 
-def direction(from_cell, to_cell):
-    if to_cell[0] > from_cell[0]:
-        direction = MOVE_S
-    elif to_cell[0] < from_cell[0]:
-        direction = MOVE_N
-    elif to_cell[1] < from_cell[1]:
-        direction = MOVE_O
-    else:
-        direction = MOVE_E
-    return direction
+def direction(from_cell, to_cell): ...
 
 
-def manhattan_distance(cell1, cell2):
-    return abs(cell1[0] - cell2[0]) + abs(cell1[1] - cell2[1])
+def manhattan_distance(cell1, cell2): ...
